ee4c5b245c0d9c45e292a0b37177fa2550b4176a
Sinthu <kumarasamy.s@husky.neu.edu>
2020-06-07 19:07:55 -0400

    Updates

---
+++ precache-manifest.a171147a860edc5df1ca0462be4e5d09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07b07b638b5cf3f76caed969672634d1",
+    "url": "/would_you_rather/index.html"
+  },
+  {
+    "revision": "63fff6107adff744e390",
+    "url": "/would_you_rather/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7942fe1bfe52609d5e85",
+    "url": "/would_you_rather/static/css/main.6e07db41.chunk.css"
+  },
+  {
+    "revision": "63fff6107adff744e390",
+    "url": "/would_you_rather/static/js/2.ff6f113c.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/would_you_rather/static/js/2.ff6f113c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7942fe1bfe52609d5e85",
+    "url": "/would_you_rather/static/js/main.596bbed8.chunk.js"
+  },
+  {
+    "revision": "ef9f1dc44e76535b5c83",
+    "url": "/would_you_rather/static/js/runtime-main.edd562f8.js"
+  }
+]);
